892c881cab6a3e68e286b72fc149ea922db3d8b1
anton.kostiuchkov <anton.kostiuchkov@collibra.com>
2019-05-05 16:18:38 +0200

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_122119) do
+ActiveRecord::Schema.define(version: 2019_05_05_135809) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
