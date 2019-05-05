84de11f0ea15303f5a2d2da07c6f13827e4be37d
anton.kostiuchkov <anton.kostiuchkov@collibra.com>
2019-05-05 16:21:47 +0200

    add before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:edit, :update, :show, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -19,12 +21,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to @post
     else
@@ -33,11 +32,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to posts_path
@@ -51,6 +48,9 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content)
   end
 
+  def find_post
+    @post = Post.find(params[:id])
+  end
   
 
 end
